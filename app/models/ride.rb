5801d876bc417bafd327b0083b78bc5c449f8ac6
Simon Krol <simonkrol11@gmail.com>
2018-02-25 04:03:01 -0500

    remove API to be moved to seperate repo

--- app/models/ride.rb
+++
@@ -1,7 +0,0 @@
-class Ride < ApplicationRecord
-  belongs_to :user
-  validates :startL, presence: {message: 'startL must be unique'}
-  validates :endL, presence:{message: 'endL must be unique'}
-  validates :startT, presence:{message: 'startT must be unique'}
-  validates :endT, presence:{message: 'endT must be unique'}
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-	post '/query' => 'application#query'
-	post '/api' => 'application#api'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
